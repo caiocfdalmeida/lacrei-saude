af4b5a441480acf8e471d2d0ec7c9a8bff83e06c
Caio Cardoso <caiofroes@hotmail.com>
2025-05-24 17:24:50 -0300

    novo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,1 @@
-import type { AppProps } from 'next/app';
-import { GlobalStyle } from '../styles/GlobalStyle';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
-  );
-}
